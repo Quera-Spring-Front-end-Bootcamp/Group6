139e2a79c9a22a149d53cccfa059b43a6ca3df01
Zahra Kaviani <zahrakvn1996@gmail.com>
2023-05-17 21:48:49 +0330

    i've changed some forms of app.tsx file. i've added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Index from "./pages/Index/Index";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Forget from "./pages/Forget/Forget";
 import Reset from "./pages/Reset/Reset";
 
-function Routes() {
-  return (
-    <div className="font-dana">
-      <Route path="/" Component={Index} />
-      <Route path="/register" Component={Register} />
-      <Route path="/login" Component={Login} />
-      <Route path="/forget" Component={Forget} />
-      <Route path="/reset" Component={Reset} />
-    </div>
-  );
+interface RoutesProps {
+  children?: React.ReactNode;
+  location?: Partial<Location> | string;
 }
 
 function App() {
   return (
-    <div>
-      <Routes />
-    </div>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/forget" element={<Forget />} />
+      <Route path="/reset" element={<Reset />} />
+    </Routes>
   );
 }
 
