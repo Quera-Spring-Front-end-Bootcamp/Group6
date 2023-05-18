31f797a01cb78d3b567561bb9470fd887f3aff07
Zahra Kaviani <zahrakvn1996@gmail.com>
2023-05-18 22:21:51 +0330

    types.ts files moved and app file is modified

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,22 @@ import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
 import Forget from "./pages/Forget/Forget";
 import Reset from "./pages/Reset/Reset";
-
-interface RoutesProps {
-  children?: React.ReactNode;
-  location?: Partial<Location> | string;
-}
+import { AppRouteProps } from "./types/types"; // update the import statement for AppRouteProps
 
 function App() {
+  const exampleElement = <div>Example component</div>; // define example element here
+
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/forget" element={<Forget />} />
-      <Route path="/reset" element={<Reset />} />
-    </Routes>
+    <div>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forget" element={<Forget />} />
+        <Route path="/reset" element={<Reset />} />
+        <Route path="/example" element={<Route {...{ path: "/example", element: exampleElement }} />} /> // example route using the Route component
+      </Routes>
+    </div>
   );
 }
 
