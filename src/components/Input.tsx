85b87a38943357f42327276f1f3117dfdcc1a313
smdmori <mortezasamadi1371@gmail.com>
2023-05-20 23:13:21 +0330

    add input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,17 @@
-export const Input = () => {
-  return <>Input</>
+type InputData = {
+  id: string
+  label: string
+  type: string
+  register: object
+}
+
+export const Input = ({ type, id, label, register }: InputData) => {
+  return (
+    <div className="flex flex-col mt-4">
+      <label className="mb-2" htmlFor={id}>
+        {label}
+      </label>
+      <input className="border border-gray-400 rounded-md mb-5 h-10 outline-none pr-1" type={type} id={id} {...register} />
+    </div>
+  )
 }
