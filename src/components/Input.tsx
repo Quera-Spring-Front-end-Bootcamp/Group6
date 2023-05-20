666809af663f5a41b3c937cc2df4d2c9ee835fd5
smdmori <mortezasamadi1371@gmail.com>
2023-05-20 23:45:13 +0330

    add chechbox input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,11 +7,14 @@ type InputData = {
 
 export const Input = ({ type, id, label, register }: InputData) => {
   return (
-    <div className="flex flex-col mt-4">
-      <label className="mb-2" htmlFor={id}>
-        {label}
-      </label>
-      <input className="border border-gray-400 rounded-md mb-5 h-10 outline-none pr-1" type={type} id={id} {...register} />
+    <div className="flex flex-col mt-5">
+      {type !== 'checkbox' && <>
+        <label className="mb-2" htmlFor={id}>
+          {label}
+        </label>
+        <input className="border border-gray-400 rounded-md h-10 outline-none pr-1" type={type} id={id} {...register} /> </>}
+
+      {type === 'checkbox' && <div className="flex "><input type="checkbox" className="ml-2" /><span>قوانین و مقررات را می‌پذیرم</span></div>}
     </div>
   )
 }
