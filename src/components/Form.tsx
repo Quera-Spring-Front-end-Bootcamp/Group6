d64aaeedc7a7708585669f328cb3dcb52aeb658a
smdmori <mortezasamadi1371@gmail.com>
2023-05-22 23:35:07 +0330

    add method and action

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,7 +2,8 @@ import { SubmitHandler, useForm } from "react-hook-form"
 import { Input } from "./Input"
 
 type FormTypes = {
-  title: string
+  action: string
+  method: 'GET' | 'POST'
 }
 
 type FormInputsData = {
@@ -11,14 +12,31 @@ type FormInputsData = {
   checkbox?: boolean
 }
 
-export const Form = ({ title }: FormTypes) => {
+export const Form = ({ action, method }: FormTypes) => {
   const { register, handleSubmit } = useForm()
 
-  const onSubmit: SubmitHandler<FormInputsData> = data => console.log('Form > SubmitHandler:', data)
+  const onSubmit: SubmitHandler<FormInputsData> = data => {
+    console.log('Form > SubmitHandler:', data)
+    sendRequest(action, method)
+  }
+
+  async function sendRequest(url: string, method: string) {
+    try {
+      const response = await fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const jsonData = await response.json()
+      return jsonData
+    } catch (error) {
+      console.error('Form > sendRequest error', error);
+    }
+  }
 
   return (
     <form dir="rtl" className="flex flex-col shadow-[0_12px_50px_rgba(0,0,0,0.18)] rounded-[20px] bg-white p-6" onSubmit={handleSubmit(onSubmit)}>
-      <h1 className="text-center font-semibold text-[2rem] leading-[50px]">{title}</h1>
 
       {/* example for adding email input */}
       <Input id="email" elementType="email" label="ایمیل" register={register("email", { required: true })} />
@@ -28,8 +46,6 @@ export const Form = ({ title }: FormTypes) => {
 
       {/* example for adding checkbox input */}
       <Input elementType="checkbox" id="checkbox" register={register('checkbox', { required: true })} label='قوانین و مقررات را می‌پذیرم' />
-
-      <button type='submit'>ورود</button>
     </form>
   )
 }
