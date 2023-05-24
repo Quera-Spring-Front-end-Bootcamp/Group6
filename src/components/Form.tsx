9b99b08580ca34fb2cbc3f96859f5fcbfc564bfb
AmirAabibi79 <69202167+AmirAabibi79@users.noreply.github.com>
2023-05-24 15:51:47 +0330

    Make form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,12 @@
-export const Form = () => {
-  return <>Form</>
-}
+import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
+import { Children } from "../types/Children";
+export const Form = ({ children }: Children) => {
+  const methods = useForm();
+  const onSubmit: SubmitHandler<any> = (data) => console.log(data);
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
+};
