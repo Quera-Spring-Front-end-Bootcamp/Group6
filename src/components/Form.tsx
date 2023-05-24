0b9854803caed421bfb9b3c5629d55e649508c0d
AmirAabibi79 <69202167+AmirAabibi79@users.noreply.github.com>
2023-05-24 17:12:02 +0330

    Add types and props

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,29 @@
 import { useForm, SubmitHandler, FormProvider } from "react-hook-form";
-import { Children } from "../types/Children";
-export const Form = ({ children }: Children) => {
+import { FormTypes } from "../types/Form";
+export const Form: React.FC<FormTypes> = ({
+  children,
+  onSuccess,
+  onError,
+  action,
+  ...props
+}) => {
   const methods = useForm();
-  const onSubmit: SubmitHandler<any> = (data) => console.log(data);
-
+  const onSubmit: SubmitHandler<any> = (data) => Fetch(data);
+  const Fetch = (data: JSON) => {
+    fetch(action, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  };
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+      <form onSubmit={methods.handleSubmit(onSubmit)} {...props}>
+        {children}
+      </form>
     </FormProvider>
   );
 };
