0222edb95896c37fcc26b2a755a75b3ab472c8c5
AmirAabibi79 <69202167+AmirAabibi79@users.noreply.github.com>
2023-05-26 22:09:54 +0330

    Create AuthCart and change style in AuthLayout

--- src/layout/Auth/AuthLayout.tsx
+++ src/layout/Auth/AuthLayout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({ children }: Children) {
     <div className="w-screen h-screen">
       <AuthHeader />
       <div className="w-screen h-screen absolute top-0 left-0 bg-gradient-[269.55deg] from-[#06846F] from-[0.35%] to-[#54BEE8] to-[103.4%] z-0 clip" />
-      <div className="w-full h-full flex items-center justify-center relative z-10">
+      <div className="w-full h-fit mt-10 flex  justify-center relative z-10">
         {children}
       </div>
     </div>
