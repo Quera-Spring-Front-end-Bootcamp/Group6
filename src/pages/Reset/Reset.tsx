e6287e8b641d1394e1c4ca3c66758861a41d2e9a
AmirAabibi79 <69202167+AmirAabibi79@users.noreply.github.com>
2023-05-27 23:02:27 +0330

    Create reset page

--- src/pages/Reset/Reset.tsx
+++ src/pages/Reset/Reset.tsx
@@ -1,7 +1,20 @@
-
+import {
+  AuthCart,
+  AuthCartHeader,
+} from "../../components/AuthPagesComps/AuthCart";
+import AuthLayout from "../../layout/Auth/AuthLayout";
 
 function Reset() {
-  return <h1>Reset</h1>;
+  return (
+    <AuthLayout>
+      <AuthCart>
+        <AuthCartHeader header="فراموشی رمز عبور" />
+        <span className="font-[400] text-[14px] text-black">
+          .لینک بازیابی رمز عبور برای شما ایمیل شد. لطفا ایمیل خود را بررسی کنید
+        </span>
+      </AuthCart>
+    </AuthLayout>
+  );
 }
 
 export default Reset;
