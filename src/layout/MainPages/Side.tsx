88f0e7324e5c77f451926e12688fc5e52f047abf
AmirAabibi79 <69202167+AmirAabibi79@users.noreply.github.com>
2023-05-31 16:01:38 +0330

    Add conditional rendering for side bar based on the page

--- src/layout/MainPages/Side.tsx
+++ src/layout/MainPages/Side.tsx
@@ -1,10 +1,21 @@
+import SideBar from "../../components/BoardPagesComps/SideBar";
 import Title from "../../components/Title";
+import { useLocation } from "react-router-dom";
 export default function Side() {
+  const location = useLocation();
   return (
-    <div className="w-[340px] pl-4 border-l-[0.5px] border-l-[#AAAAAA] py-10">
-      <div className="pb-5 flex justify-end">
+    <div
+      dir="rtl"
+      className="w-[340px] pl-4 border-l-[0.5px] border-l-[#AAAAAA] py-10"
+    >
+      <div className="pb-5">
         <Title />
       </div>
+      {location.pathname !== "/profile" ? (
+        <SideBar />
+      ) : (
+        <div>profile side bar</div>
+      )}
     </div>
   );
 }
