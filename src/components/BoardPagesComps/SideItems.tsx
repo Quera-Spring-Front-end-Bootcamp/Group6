c648ce688b0f8c839c2779cd433552621e06afd9
AmirAabibi79 <69202167+AmirAabibi79@users.noreply.github.com>
2023-06-03 20:41:58 +0330

    Change style

--- src/components/BoardPagesComps/SideItems.tsx
+++ src/components/BoardPagesComps/SideItems.tsx
@@ -3,10 +3,10 @@ import SideItem from "./SideItem";
 export default function SideItems() {
   return (
     <div className="h-[300px] overflow-y-auto flex flex-col gap-4">
-      <SideItem title="درس مدیریت پروژه" color={"#71FDA9"} selected={true} />
-      <SideItem title="درس مدیریت پروژه" color={"#71FDA9"} selected={false} />
-      <SideItem title="درس مدیریت پروژه" color={"#71FDA9"} selected={false} />
-      <SideItem title="درس مدیریت پروژه" color={"#71FDA9"} selected={false} />
+      <SideItem title="درس مدیریت پروژه" color={"#EAF562"} selected={true} />
+      <SideItem title="درس مدیریت پروژه" color={"#EAF562"} selected={false} />
+      <SideItem title="درس مدیریت پروژه" color={"#EAF562"} selected={false} />
+      <SideItem title="درس مدیریت پروژه" color={"#EAF562"} selected={false} />
     </div>
   );
 }
